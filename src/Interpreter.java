c8e5e9d43e27a763c456c519473de15f42fe00c8
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2017-07-06 00:25:23 +0200

    AM0Command

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -6,10 +6,34 @@ import java.nio.file.Paths;
  */
 public class Interpreter {
 
-    public static void main(String[] argv)
-    {
+    public static void main(String[] argv) {
+
+        System.out.println("Program start..");
+
+        String[] program = {
+                "READ 1",
+                "READ 2",
+                "LOAD 1",
+                "LOAD 2",
+                "GT",
+                "JMC 9",
+                "LOAD 1",
+                "STORE 3",
+                "JMP 11",
+                "LOAD 2",
+                "STORE 3",
+                "WRITE 3"
+        };
+
+        StackPointer stackPointer = StackPointer.getInstance();
+        AM0Command command = new AM0Command();
 
 
+        while(stackPointer.getValue() < program.length) {
 
+            command.compileAndExecute(program[stackPointer.getValue()]);
+        }
+
+        System.out.println("Program exec..");
     }
 }
