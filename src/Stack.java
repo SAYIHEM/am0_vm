117d6700661657dd1105d3f0e28ae563d34b5d92
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2017-07-05 20:34:30 +0200

    stack implemeted

---
+++ src/Stack.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+/**
+ * Created by X0R_R0X on 7/5/2017.
+ */
+public class Stack {
+    private static Stack instance = new Stack();
+    public Stack getInstance()
+    {
+        return instance;
+    }
+
+    private ArrayList<Integer> stack = new ArrayList<>();
+
+    //throws StackExceptions
+    public Integer pop()
+    {
+        int i = stack.size() - 1;
+        if(i < 0)
+        {
+            throw new StackExceptions("negative stackpointer! stack empty.");
+        }
+        Integer value = stack.get(i);
+        stack.remove(i);
+        return value;
+    }
+
+    public void push(Integer integer)
+    {
+        stack.add(integer);
+    }
+}
---
+++ src/StackException.java
@@ -0,0 +1,9 @@
+/**
+ * Created by X0R_R0X on 7/5/2017.
+ */
+public class StackException extends RuntimeException {
+    public StackException(String message)
+    {
+        System.out.println(message);
+    }
+}
