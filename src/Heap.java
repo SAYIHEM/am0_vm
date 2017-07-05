e4f582410e302cf97acac02cf975f0276119e223
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2017-07-05 20:47:45 +0200

    stack implemeted

---
+++ src/Heap.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by X0R_R0X on 7/5/2017.
+ */
+public class Heap {
+    private static Heap instance = new Heap();
+    public Heap getInstance()
+    {
+        return instance;
+    }
+
+    private Integer[] heap = new Integer[100];
+
+}
