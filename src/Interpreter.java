038b4d00adbe2c03f8f8ce9b67e212be3447be9f
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2017-07-06 00:13:23 +0200

    AM0Command

---
+++ src/Interpreter.java
@@ -0,0 +1,15 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by X0R_R0X on 7/6/2017.
+ */
+public class Interpreter {
+
+    public static void main(String[] argv)
+    {
+
+
+
+    }
+}
