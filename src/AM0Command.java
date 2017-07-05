be3cd09c6c7dc3c0727ce24c343965ed37668690
tomg <tom.grassmann@live.de>
2017-07-05 22:52:55 +0200

    AM0Command

--- src/AM0Command.java
+++ src/AM0Command.java
@@ -1,2 +1,12 @@
-public class AM0Command {
+public class AM0Command implements Command {
+
+    public AM0Command() {
+
+
+    }
+
+    @Override
+    public void compileAndExecute(String argument) {
+
+    }
 }
