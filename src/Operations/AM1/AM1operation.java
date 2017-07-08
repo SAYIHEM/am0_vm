88127c11be07859eacf6b52601373cdc1690d7a8
tomg <tom.grassmann@live.de>
2017-07-08 20:49:28 +0200

    .

---
+++ src/Operations/AM1/AM1operation.java
@@ -0,0 +1,40 @@
+package Operations.AM1;
+
+import Hardware.CommandPointers.CommandPointer;
+import Hardware.Heaps.Heap;
+import Hardware.Stacks.Pointer;
+import Hardware.Stacks.Stack;
+import Operations.Operation;
+
+public abstract class AM1operation extends Operation {
+
+    protected Heap heap;
+    protected Stack stack;
+    protected CommandPointer commandPointer;
+    protected Pointer reference;
+
+    public AM1operation(Heap heap, Stack stack, CommandPointer commandPointer, Pointer reference) {
+
+        this.name = this.getClass().getSimpleName();
+
+        this.heap = heap;
+        this.stack = stack;
+        this.commandPointer = commandPointer;
+        this.reference = reference;
+    }
+
+    @Override
+    protected void increment() {
+
+        commandPointer.increment();
+    }
+
+    protected String[] GLOBAL_LOKAL(String arg) {
+
+        arg = arg.replace("(", "");
+        arg = arg.replace(")", "");
+        arg = arg.replace(",", "");
+
+        return arg.split(" ");
+    }
+}
