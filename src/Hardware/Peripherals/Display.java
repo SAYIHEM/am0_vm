f510a82006a5e2887a64240cb1fcebe533516c4a
IamTheOneWhoKnocks <vjvsp@yahoo.de>
2017-07-08 14:48:13 +0200

    adding virtual peripherals

---
+++ src/Hardware/Peripherals/Display.java
@@ -0,0 +1,7 @@
+package Hardware.Peripherals;
+
+/**
+ * Created by X0R_R0X on 7/8/2017.
+ */
+public class Display {
+}
---
+++ src/Hardware/Peripherals/Soundcard.java
@@ -0,0 +1,14 @@
+package Hardware.Peripherals;
+
+import Hardware.Heaps.Heap;
+
+/**
+ * Created by X0R_R0X on 7/8/2017.
+ */
+public class Soundcard {
+    private int frequencyAddress, timeAddress;
+    private Heap sharedStorage;
+
+    public Soundcard(Heap sharedStorage, int frequencyAddress, int timeAddress)
+
+}
